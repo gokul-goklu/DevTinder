96a9cab6d313ed32979f683f2c15df00fbe998fa
LAPTOP-O72GM1HI\gokul <gokulshanmugam062@gmail.com>
2025-06-01 08:02:18 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+
+app.use("/data",(req,res)=>{
+    res.send("It is a message from data page")
+})
+
+app.use("/help",(req,res)=>{
+  res.send("this is from help page")
+})
+
+app.use((req,res)=>{
+    res.send("The base page is not  loaded")
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is done !!")
+})
