37f5dca9a61ff3631919298b3a33611235e4c03e
LAPTOP-O72GM1HI\gokul <gokulshanmugam062@gmail.com>
2025-06-01 14:50:22 +0530

    get and post

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const express=require("express")
 const app=express()
 
-app.use("/data",(req,res)=>{
-    res.send("It is a message from data page")
+// app.use("/data",(req,res)=>{
+//     res.send("It is a message from data page")
+// })
+
+// app.use("/help",(req,res)=>{
+//   res.send("this is from help page")
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("The base page is not  loaded")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send("this is specific to get user")
 })
 
-app.use("/help",(req,res)=>{
-  res.send("this is from help page")
-})
 
-app.use((req,res)=>{
-    res.send("The base page is not  loaded")
+app.post("/user",(req,res)=>{
+    res.send("Data is saved in the DB")
 })
 
 
